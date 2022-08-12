51998095da5f713271ea97c07a31dcf60d2f187f
MohamdHajjRabee <a39630595@gmail.com>
2022-08-12 12:28:27 +0300

    init commit

---
+++ NoNameProject/javscript.js
@@ -0,0 +1,48 @@
+
+new TypeIt("#home-text-span", {
+    strings: "Coding is life",
+    speed: 175,
+    loop: true,
+    loopDelay: 5000
+}).go();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//
+// let letter = 0;
+// let speed = 0;
+// function typeAnimate(){
+//     let text = "Coding is life";
+//     if (letter < text.length){
+//         document.getElementById("home-text-span").innerHTML += text.charAt(letter);
+//         letter++;
+//         speed = 150;
+//
+//     }
+//
+//     setTimeout(typeAnimate,speed);
+// }
+//
+//
+//
+//
+//
+// typeAnimate()
+
+
+
+
